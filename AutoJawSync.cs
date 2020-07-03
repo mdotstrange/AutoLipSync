b9daa292b175b5db2fa14bab52eb8c5547082b4f
M dot Strange <mdotstrange@gmail.com>
2020-07-03 14:36:57 -0700

    Add files via upload

---
+++ AutoJawSync.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class AutoJawSync : MonoBehaviour
+{
+
+    public AudioSource AudioSource;
+    public GameObject jaw;
+    [Range(1, 50)]
+    public float JawSensitivity;
+    public float ClosedJawMax;
+    public float OpenJawMax;
+    float loud;
+    float lastLoud;
+    float smoothedLoudness;
+    float clampedJawFloat;
+    [Header("For debugging")]
+    public Quaternion LiveJawRotationOutput;
+    [Space]
+    [Header("Fires an event when talking starts")]
+    public UnityEvent FireOnStartedTalking;
+
+    void Update()
+    {     
+            SyncUpdate();
+    }
+
+    public void SyncUpdate()
+    {
+        if (AudioSource == null)
+        {
+            lastLoud = 0f;
+            return;
+        }
+
+        if (AudioSource.isPlaying)
+        {
+            ProcessAudio();
+        }
+        else
+        {
+            lastLoud = 0f;
+        }
+    }
+
+    void ProcessAudio()
+    {
+        loud = GetAveragedVolume() * JawSensitivity;
+
+        if (loud >= 0.91f && lastLoud <= 0.9f)
+        {
+            FireOnStartedTalking.Invoke();
+        }
+
+        smoothedLoudness = lowPassFilter(loud, ref smoothedLoudness, 0.8f, false);
+
+        Vector3 NewJawRotationAsEuler = new Vector3(DoFloatRemap(), 0, 0);
+        //LiveJawRotationOutput = Quaternion.Euler(NewJawRotationAsEuler);
+
+        //Debug.Log("w = " + LiveJawRotationOutput.w);
+        //Debug.Log("x = " + LiveJawRotationOutput.x);
+        //Debug.Log("y = " + LiveJawRotationOutput.y);
+        //Debug.Log("z = " + LiveJawRotationOutput.z);
+
+        jaw.transform.localEulerAngles = NewJawRotationAsEuler;
+        lastLoud = loud;
+    }
+
+    float DoFloatRemap()
+    {
+        return map(smoothedLoudness, 0, 4, ClosedJawMax, OpenJawMax);
+    }
+
+    float map(float s, float a1, float a2, float b1, float b2)
+    {
+        return b1 + (s - a1) * (b2 - b1) / (a2 - a1);
+    }
+
+    float GetAveragedVolume()
+    {
+        float[] data = new float[256];
+        float a = 0;
+        AudioSource.GetOutputData(data, 0);
+        foreach (float s in data)
+        {
+            a += Mathf.Abs(s);
+        }
+        return a / 256;
+    }
+
+    float lowPassFilter(float targetValue, ref float intermediateValueBuf, float factor, bool init)
+    {
+
+        float intermediateValue;
+
+        //intermediateValue needs to be initialized at the first usage.
+        if (init)
+        {
+            intermediateValueBuf = targetValue;
+        }
+
+        intermediateValue = (targetValue * factor) + (intermediateValueBuf * (1.0f - factor));
+
+
+        intermediateValueBuf = intermediateValue;
+
+        return intermediateValue;
+    }
+}
---
+++ PlayAudioWithLipSync.cs
@@ -0,0 +1,158 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Slate.ActionClips
+{
+
+    [Name("Audio Clip with Lip sync")]
+    [Description("The audio clip will be send to the AudioMixer selected in it's track if any. You can trim or loop the audio by scaling the clip and you can optionaly show subtitles as well.")]
+    [Attachable(typeof(ActorAudioTrack), typeof(DirectorAudioTrack))]
+    public class PlayAudioWithLipSync : ActionClip, ISubClipContainable
+    {
+
+        [SerializeField]
+        [HideInInspector]
+        private float _length = 1f;
+        [SerializeField]
+        [HideInInspector]
+        private float _blendIn = 0.25f;
+        [SerializeField]
+        [HideInInspector]
+        private float _blendOut = 0.25f;
+
+        [Required]
+        public AudioClip audioClip;
+        [AnimatableParameter(0, 1)]
+        public float volume = 1;
+        [AnimatableParameter(-3, 3)]
+        public float pitch = 1;
+        [AnimatableParameter(-1, 1)]
+        public float stereoPan = 0;
+
+        public float clipOffset;
+        [Multiline(5)]
+        public string subtitlesText;
+        public Color subtitlesColor = Color.white;
+
+        public AutoJawSync AutoJawSync;
+
+        float ISubClipContainable.subClipOffset
+        {
+            get { return clipOffset; }
+            set { clipOffset = value; }
+        }
+
+        float ISubClipContainable.subClipLength
+        {
+            get { return audioClip != null ? audioClip.length : 0; }
+        }
+
+        float ISubClipContainable.subClipSpeed
+        {
+            get { return 1; }
+        }
+
+        public override float length
+        {
+            get { return _length; }
+            set { _length = value; }
+        }
+
+        public override float blendIn
+        {
+            get { return _blendIn; }
+            set { _blendIn = value; }
+        }
+
+        public override float blendOut
+        {
+            get { return _blendOut; }
+            set { _blendOut = value; }
+        }
+
+        public override bool isValid
+        {
+            get { return audioClip != null; }
+        }
+
+        public override string info
+        {
+            get { return isValid ? (string.IsNullOrEmpty(subtitlesText) ? audioClip.name : string.Format("<i>'{0}'</i>", subtitlesText)) : base.info; }
+        }
+
+        private AudioTrack track
+        {
+            get { return (AudioTrack)parent; }
+        }
+
+        private AudioSource source
+        {
+            get { return track.source; }
+        }
+
+        protected override void OnEnter() { Do(); }
+        protected override void OnReverseEnter() { Do(); }
+        protected override void OnExit() { Undo(); }
+        protected override void OnReverse() { Undo(); }
+
+        void Do()
+        {
+            if (source != null)
+            {
+                source.clip = audioClip;
+
+                if (AutoJawSync != null)
+                {
+                    AutoJawSync.AudioSource = source;
+                }
+
+            }
+        }
+
+        protected override void OnUpdate(float time, float previousTime)
+        {
+            if (source != null)
+            {
+                var weight = Easing.Ease(EaseType.QuadraticInOut, 0, 1, GetClipWeight(time));
+                var settings = track.sampleSettings;
+                settings.volume *= weight * volume;
+                settings.pitch *= pitch;
+                settings.pan += stereoPan;
+
+                AudioSampler.Sample(source, audioClip, time - clipOffset, previousTime - clipOffset, settings);
+
+                if (AutoJawSync != null)
+                {
+                    AutoJawSync.SyncUpdate();
+                }
+
+                if (!string.IsNullOrEmpty(subtitlesText))
+                {
+                    var lerpColor = subtitlesColor;
+                    lerpColor.a = weight;
+                    DirectorGUI.UpdateSubtitles(string.Format("{0}{1}", parent is ActorAudioTrack ? (actor.name.Replace("(Clone)", "") + ": ") : "", subtitlesText), lerpColor);
+                }
+            }
+        }
+
+        void Undo()
+        {
+            if (source != null)
+            {
+                source.clip = null;
+            }
+        }
+
+        ////////////////////////////////////////
+        ///////////GUI AND EDITOR STUFF/////////
+        ////////////////////////////////////////
+#if UNITY_EDITOR
+
+        protected override void OnClipGUI(Rect rect)
+        {
+            EditorTools.DrawLoopedAudioTexture(rect, audioClip, length, clipOffset);
+        }
+
+#endif
+    }
+}
